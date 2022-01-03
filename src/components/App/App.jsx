b8d0a5bdda655ce1aa532d7938ecfc7abfe0bcab
Lucas Alves Antunes <lucas.antunes@meliuz.com.br>
2022-01-03 19:16:10 -0300

    chore: Adiciona shared component Checkbox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppHeader from '../AppHeader/AppHeader';
 import AppContainer from '../AppContainer/AppContainer';
 import { Wrapper, Container } from './App.styles';
+import Checkbox from '../../shared/Checkbox/Checkbox';
 
 function App() {
+  const [alface, setAlface] = useState();
   return (
     <Wrapper>
       <Container>
         <AppHeader />
         <AppContainer
-          left={<div>Produtos Disponíveis</div>}
-          middle={<div>Sua Lista de Compras</div>}
+          left={
+            <div>
+              Produtos disponíveis:{' '}
+              <Checkbox
+                value={alface}
+                title='Alface'
+                onClick={() => setAlface(!alface)}
+              />
+            </div>
+          }
+          middle={<div>Sua lista de compras</div>}
           right={<div>Estatísticas</div>}
         />
       </Container>
